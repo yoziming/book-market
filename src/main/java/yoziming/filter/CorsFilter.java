2103267189f5ab7c23ef82ec79fb5a51a092f04d
Yoziming <yoziming@gmail.com>
2022-02-10 19:20:12 +0800

    Ready to deploy

--- src/main/java/yoziming/filter/CorsFilter.java
+++ src/main/java/yoziming/filter/CorsFilter.java
@@ -1,32 +1,39 @@
 package yoziming.filter;
 
-import org.apache.commons.httpclient.HttpStatus;
-
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URL;
 
 public class CorsFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
+    private static final String HTTPS = "https";
+    private static final int HTTPS_PORT = 8443;
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        // 指定允許其他域名訪問
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        // 響應型別
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS, DELETE");
-        // 響應頭設定
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header, " +
-                "HaiYi-Access-Token");
-        if ("OPTIONS".equals(request.getMethod())) {
-            response.setStatus(HttpStatus.SC_NO_CONTENT);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        URL newUrl = null;
+        if (request.getScheme().equals(HTTPS)) {
+            chain.doFilter(request, response);
+        } else {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            String queryString = httpRequest.getQueryString() == null ? "" : "?" + httpRequest.getQueryString();
+            httpResponse.setStatus(307);
+            String requestUrl = httpRequest.getRequestURL().toString();
+            URL reqUrl = new URL(requestUrl + queryString);
+            newUrl = new URL(HTTPS, reqUrl.getHost(), HTTPS_PORT, reqUrl.getFile());
+            //進行重定向
+            httpResponse.setHeader("Location", newUrl.toString());
+            httpResponse.setHeader("Connection", "close");
+            //允許所有跨域請求
+            httpResponse.addHeader("Access-Control-Allow-Origin", "*");
         }
-        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
